8ff8131a0504e9d7d38e82b511e662e30adfab15
BNKDuy <100258881+BNKDuy@users.noreply.github.com>
2022-02-24 20:41:05 -0500

    feb 24

--- src/javaapplication59/JavaApplication59.java
+++ src/javaapplication59/JavaApplication59.java
@@ -13,8 +13,8 @@ public class JavaApplication59 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int v1 = 0, r1 = 0, u = 0;
-        String in, out = "";
+        int v1 = 0, r1 = 0, u = 0, c = 0;
+        String in, out1 = "", out2 = "";
 
         Scanner input = new Scanner(System.in);
 
@@ -26,7 +26,7 @@ public class JavaApplication59 {
         {
           if ((i == 0) || (i > 0 &&(in.substring(i - 1, i).equals(" "))) || (!(in.substring(i, i+ 1).equals("a") || in.substring(i, i + 1).equals("e") || in.substring(i, i + 1).equals("i") || in.substring(i, i + 1).equals("o") || in.substring(i, i + 1).equals("u"))) && (!(in.substring(i, i+1).equals(in.substring(i - 1, i)))))
           {
-            out += in.substring(i, i + 1);
+            out1 += in.substring(i, i + 1);
           }
           else
           {
@@ -43,12 +43,31 @@ public class JavaApplication59 {
 
         System.out.println("Vowels removed: " + v1);
         System.out.println("Repeats removed: " + r1);
-        System.out.println("Algorithm 1 message: " + out);
+        System.out.println("Algorithm 1 message: " + out1);
         System.out.println("Algorithm 1 characters saved: " + (v1 + r1));
 
         System.out.println("\nAlgorithm 2");
 
-        
+        for (int i = 0; i < in.length(); i++)
+        {
+          if (!(in.substring(i,i+1).equals(" ")))
+          {
+            for (int t = 0; t < in.length(); t++)
+            {
+              if (in.substring(t, t+1).equals(in.substring(i, i+1)))
+              {
+                c++;
+              }
+            }
+            u++;
+            out2 += c;
+            out2 += in.substring(i,i+1);
+            c = 0;
+            in = in.replaceAll((in.substring(i,i+1)), " ");
+          }
+        }
+        System.out.println("Unique characters found: " + u);
+        System.out.println("Algorithm 2 message: " + out2);
+        System.out.println("Algorithm 2 characters saved: " + (in.length() - out2.length()));
     }
-    
 }
